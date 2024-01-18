2e1c3dd549edda63bc045eada373a2bd8c08ca4c
dharma putra <dharma.putra@dwp.co.id>
2024-01-18 16:37:17 +0700

    update signinn page

--- lib/pages/sign_in/sign_in.dart
+++ lib/pages/sign_in/sign_in.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_rust_complete/pages/sign_in/widgets/sign_in_widget.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class SignIn extends StatefulWidget {
   const SignIn({super.key});
@@ -17,6 +18,26 @@ class _SignInState extends State<SignIn> {
         child: Scaffold(
           backgroundColor: Colors.white,
           appBar: buildAppBar(),
+          body: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                buildThridPartyLogin(context),
+                const Center(
+                  child: reusableText(text: "Or use your email account login"),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 66.h),
+                  padding: EdgeInsets.only(left: 25.w),
+                  child: Column(
+                    children: [
+                      reusableText(text: "Email"),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
